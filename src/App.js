0642d4b9fc2bb6f32e93407dda369e823f6b3863
Bharat Dua <bharatdua13@gmail.com>
2024-06-27 23:24:50 +0530

    required components for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./index.css";
-
+function Logo() {
+  return <h1>FAR AWAY</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">PackingList</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X </em>
+    </footer>
+  );
+}
 function App() {
-  return <h1>react</h1>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
