c666a4ad4c09588530c5ac507707f0a97af219a2
Bharat Dua <bharatdua13@gmail.com>
2024-07-01 11:36:44 +0530

    added footer stats

--- src/App.js
+++ src/App.js
@@ -63,10 +63,24 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
     </div>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100) || 0;
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X </em>
+      <em>
+        {percentage === 100
+          ? "You got everything!Ready to go"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked}(${percentage}%)`}
+      </em>
     </footer>
   );
 }
@@ -115,7 +129,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
